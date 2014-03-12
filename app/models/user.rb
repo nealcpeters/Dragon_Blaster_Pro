92021f3a2ed62f19f73f9152c3e038113cd18c39
Daniel Lu <dandydanny@gmail.com>
2014-03-12 11:24:27 -0500

    Created User association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  has_many :maps, foreign_key: "creator_id"
+  has_many :heros, foreign_key: "player_id"
+  has_many :games, foreign_key: "player_id"
+
 end
