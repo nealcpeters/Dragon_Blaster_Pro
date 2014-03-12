a5dc7b4a361d82d847099f47ff15f33c980b6ff9
Apprentice <apprentice@dbc-chicago-007.local>
2014-03-12 11:05:38 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DragonBlaster::Application.config.secret_key_base = 'adef77b35232ecea6121569cb99fb52022f1f71d4fad8d3d5c7cc5b0adafcda90bb0c50179d4dc3b544d4d7f056fea9bbb937f27b88c255b1b0e982db0606f70'
