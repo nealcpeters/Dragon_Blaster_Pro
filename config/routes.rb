0635ee4ec849c8186316935a4e7a260fae692f53
Neal Peters <nealcpeters@gmail.com>
2014-03-12 12:42:11 -0500

    add flash notification on welcome page on sign in and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ DragonBlaster::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   resources :sessions
   resources :users
