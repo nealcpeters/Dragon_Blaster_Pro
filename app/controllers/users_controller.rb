888bce5fb654a3a7f41ecc96eb1ce90e675e5e40
calvached <calvache.diana@gmail.com>
2014-03-12 13:25:15 -0500

    linked log-in and sign-up to a user landing page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_url, notice: 'Signed Up!'
+      redirect_to users_path, notice: 'Signed Up!'
     else
       render 'new'
     end
+  end
+
+  def index
+    @user = User.find(session[:user_id])
   end
 
   private
