1e2e4edec1eddc0eef049dc071a879e3e93e69b1
Apprentice <apprentice@dbc-chicago-007.local>
2014-03-12 11:24:47 -0500

    all associated views with user login, signup, and sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    user = User.find_by_email(params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: 'Logged in!'
     else
-      flash.now.alert = 'Invalid email or password'
+      flash.now.alert = 'Invalid username or password'
       render 'new'
     end
   end
