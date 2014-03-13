6add1cabd659bf0978e04941c4c3382aa747c9a8
Neal Peters <nealcpeters@gmail.com>
2014-03-13 16:24:05 -0500

    After merge

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,6 @@
 class RoomsController < ApplicationController
 
   def create
-    puts "========================= you did it!!! ==============================="
-    puts params
-    puts room_params
     @room = Room.new(room_params)
 
     if @room.save
