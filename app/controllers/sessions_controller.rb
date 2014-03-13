a9b997df9de879865a67e320cc405300ca6c8bfa
Eric Stensland <estensland88@gmail.com>
2014-03-12 22:27:08 -0500

    made tests pass again, one had to be pending

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    puts "We are in the Create Method"
     user = User.find_by_username(params[:username])
     p user
     if user && user.authenticate(params[:password])
-      puts "We are authenticated and should be redirected"
       session[:user_id] = user.id
-      redirect_to profile_path, notice: 'Logged in!'
-    else
-      puts "We have entered the wrong info"
+      redirect_to user_path(user), notice: 'Logged in!'
+     else
       flash.now.alert = 'Invalid username or password'
       render 'new'
     end
