4bea2bc9e3ff800863cb84bbbfbcbb12f235ea57
estensland <estensland88@gmail.com>
2014-03-13 12:41:54 -0500

    Hero movement and tests added

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -10,18 +10,22 @@ class Hero < ActiveRecord::Base
   belongs_to :game
 
   def move_hero_east
-    # Hero
+    room = Room.find(self.room_id)
+    self.update(room_id: room.room_to_the_east.id)
   end
 
   def move_hero_west
-
+    room = Room.find(self.room_id)
+    self.update(room_id: room.room_to_the_west.id)
   end
 
   def move_hero_south
-
+    room = Room.find(self.room_id)
+    self.update(room_id: room.room_to_the_south.id)
   end
 
   def move_hero_north
-
+    room = Room.find(self.room_id)
+    self.update(room_id: room.room_to_the_north.id)
   end
 end
--- spec/models/hero_spec.rb
+++ spec/models/hero_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe Hero do
-
   it "should create valid hero" do
     expect{
       Hero.create(room_id: 1, game_id: 1, player_id: 1, name: 'abed', description: 'best game on earth')
@@ -38,5 +37,61 @@ describe Hero do
     }.to change(Hero, :count).by(0)
   end
 
+  context "movement methods" do
+
+    it "#move_hero_north shouldl move hero north" do
+      user = User.create(username: 'Abed', email: 'abed@greendale.com', password: 'password', password_confirmation: 'password')
+      map = Map.create(creator_id: user.id.to_i, title: "Dreamatorium", starting_room_id: 1, description: 'Where we play Professor Spacetime and make dreams come true. Come join us!')
+      room = Room.create(map_id: map.id.to_i, title: 'bob', description: 'fun fun fun')
+      room_to_north = User.first.maps.first.rooms.create(title: 'bob', north_id: "#{room.id}", description: 'fun fun fun')
+      # room_to_east = User.first.maps.first.rooms.create(title: 'bob', east_id: "#{@room.id}", description: 'fun fun fun')
+      # room_to_south = User.first.maps.first.rooms.create(title: 'bob', south_id: "#{@room.id}", description: 'fun fun fun')
+      # room_to_west = User.first.maps.first.rooms.create(title: 'bob', west_id: "#{@room.id}", description: 'fun fun fun')
+      hero = Hero.create(room_id: "#{room_to_north.id}", game_id: 1, player_id: 1, name: 'abed')
+      hero.move_hero_north
+      expect(hero.room_id == room.id).to be true
+    end
+
+    it "#move_hero_north shouldl move hero south" do
+      user = User.create(username: 'Abed', email: 'abed@greendale.com', password: 'password', password_confirmation: 'password')
+      map = Map.create(creator_id: user.id.to_i, title: "Dreamatorium", starting_room_id: 1, description: 'Where we play Professor Spacetime and make dreams come true. Come join us!')
+      room = Room.create(map_id: map.id.to_i, title: 'bob', description: 'fun fun fun')
+      room_to_south = User.first.maps.first.rooms.create(title: 'bob', south_id: "#{room.id}", description: 'fun fun fun')
+      # room_to_east = User.first.maps.first.rooms.create(title: 'bob', east_id: "#{@room.id}", description: 'fun fun fun')
+      # room_to_south = User.first.maps.first.rooms.create(title: 'bob', south_id: "#{@room.id}", description: 'fun fun fun')
+      # room_to_west = User.first.maps.first.rooms.create(title: 'bob', west_id: "#{@room.id}", description: 'fun fun fun')
+      hero = Hero.create(room_id: "#{room_to_south.id}", game_id: 1, player_id: 1, name: 'abed')
+      hero.move_hero_south
+      expect(hero.room_id == room.id).to be true
+    end
+
+    it "#move_hero_north shouldl move hero east" do
+      user = User.create(username: 'Abed', email: 'abed@greendale.com', password: 'password', password_confirmation: 'password')
+      map = Map.create(creator_id: user.id.to_i, title: "Dreamatorium", starting_room_id: 1, description: 'Where we play Professor Spacetime and make dreams come true. Come join us!')
+      room = Room.create(map_id: map.id.to_i, title: 'bob', description: 'fun fun fun')
+      room_to_east = User.first.maps.first.rooms.create(title: 'bob', east_id: "#{room.id}", description: 'fun fun fun')
+      # room_to_east = User.first.maps.first.rooms.create(title: 'bob', east_id: "#{@room.id}", description: 'fun fun fun')
+      # room_to_south = User.first.maps.first.rooms.create(title: 'bob', south_id: "#{@room.id}", description: 'fun fun fun')
+      # room_to_west = User.first.maps.first.rooms.create(title: 'bob', west_id: "#{@room.id}", description: 'fun fun fun')
+      hero = Hero.create(room_id: "#{room_to_east.id}", game_id: 1, player_id: 1, name: 'abed')
+      hero.move_hero_east
+      expect(hero.room_id == room.id).to be true
+    end
+
+    it "#move_hero_north shouldl move hero west" do
+      user = User.create(username: 'Abed', email: 'abed@greendale.com', password: 'password', password_confirmation: 'password')
+      map = Map.create(creator_id: user.id.to_i, title: "Dreamatorium", starting_room_id: 1, description: 'Where we play Professor Spacetime and make dreams come true. Come join us!')
+      room = Room.create(map_id: map.id.to_i, title: 'bob', description: 'fun fun fun')
+      room_to_west = User.first.maps.first.rooms.create(title: 'bob', west_id: "#{room.id}", description: 'fun fun fun')
+      # room_to_east = User.first.maps.first.rooms.create(title: 'bob', east_id: "#{@room.id}", description: 'fun fun fun')
+      # room_to_south = User.first.maps.first.rooms.create(title: 'bob', south_id: "#{@room.id}", description: 'fun fun fun')
+      # room_to_west = User.first.maps.first.rooms.create(title: 'bob', west_id: "#{@room.id}", description: 'fun fun fun')
+      hero = Hero.create(room_id: "#{room_to_west.id}", game_id: 1, player_id: 1, name: 'abed')
+      hero.move_hero_west
+      expect(hero.room_id == room.id).to be true
+    end
+
+
+  end
 
 end
