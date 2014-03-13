643b48afc49950a202d3615cbe46fe91ed814444
estensland <estensland88@gmail.com>
2014-03-13 14:27:00 -0500

    heros refixed

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,13 +1,10 @@
 class Hero < ActiveRecord::Base
-  validates :room_id, presence: :true
-  validates :game_id, presence: :true
   validates :player_id, presence: :true
   validates :name, presence: :true, uniqueness: :true
   validates :description, presence: :true
 
-  belongs_to :room
   belongs_to :player, class_name: 'User'
-  belongs_to :game
+  has_many :games
 
   def move_hero_east
     room = Room.find(self.room_id)
