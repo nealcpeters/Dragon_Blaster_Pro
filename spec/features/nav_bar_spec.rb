0ac6c4548366749e7a9439d7e7565778b2724567
estensland <estensland88@gmail.com>
2014-03-13 10:44:54 -0500

    nav bar navs and is tested to prove that it navs in a navving manner

--- spec/features/nav_bar_spec.rb
+++ spec/features/nav_bar_spec.rb
@@ -20,6 +20,25 @@ feature 'The Nav Bar' do
       expect(page).to have_content("Abed's Profile")
     end
 
+    it "it should nav" do
+      visit "/users/new"
+
+         fill_in 'user_username', with: "Abed"
+         fill_in 'user_email', with: "abed@greendale.com"
+         fill_in 'user_password', with: "password"
+         fill_in 'user_password_confirmation', with: "password"
+         click_button "Create User"
+
+      click_link "Abed's Profile"
+      expect(page).to have_content("Abed's Profile")
+    end
+
+    it "the title link should nav to home" do
+      visit "/users/new"
+      click_link "Dragon Blaster"
+      expect(page).to have_content("Welcome to Dragon Blaster!")
+    end
+
 
   end
 end
