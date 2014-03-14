cd0e4ed11421b1c4178b9a26717f4e35c25933e9
thejessleigh <j.l.unrein@gmail.com>
2014-03-14 10:43:27 -0500

    user can now create a new player with bio before they start playing a game.

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -5,9 +5,29 @@ class HerosController < ApplicationController
   end
 
   def index
-    @user = User.find(session[:user_id])
+    @user = current_user
     @hero = @user.heros.all
   end
 
+  def new
+    @hero = Hero.new
+    @user = current_user
+  end
 
+  def create
+    @user = current_user
+    @hero = Hero.new(hero_params.merge(player_id: current_user.id))
+    if @hero.save
+      redirect_to user_heros_path
+    else
+      flash[:alert] = "Error creating your hero. Try again"
+      render 'new'
+    end
+  end
+
+
+  private
+    def hero_params
+      params.require(:hero).permit(:name, :description)
+    end
 end
