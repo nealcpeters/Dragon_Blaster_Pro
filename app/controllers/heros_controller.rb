109f6b9cb84a967b7ed49560f095f954931f34c0
Alexandru Codreanu <alexcodreanu86@yahoo.com>
2014-03-14 09:55:26 -0500

    refactored the heroes controller #index

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,11 +1,13 @@
 class HerosController < ApplicationController
 
+
+
   def show
     @hero = Hero.find(params[:id])
   end
 
   def index
-    @user = User.find(session[:user_id])
+    @user = current_user
     @hero = @user.heros.all
   end
 
