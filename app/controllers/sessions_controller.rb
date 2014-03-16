fa98e53a187b4299e575589f72305a1c2a88e9b1
Adam Willis <null>
2014-03-15 21:21:38 -0500

    more css changes and added link to hero creation upon starting a game if you have no heroes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_username(params[:username])
-    p user
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to user_path(user), notice: 'Logged in!'
